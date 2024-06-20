69808e35c9d91945349a5a33f020df0ad860ad44
Martin Bonnin <martin@mbonnin.net>
2024-06-20 15:55:26 +0200

    use argument instead of option

--- cli/src/main/kotlin/com/gradleup/librarian/cli/PrepareReleaseCommand.kt
+++ cli/src/main/kotlin/com/gradleup/librarian/cli/PrepareReleaseCommand.kt
@@ -1,13 +1,11 @@
 package com.gradleup.librarian.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.options.help
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.arguments.argument
 import com.gradleup.librarian.core.tooling.prepareRelease
 
 internal class PrepareReleaseCommand: CliktCommand(hidden = true) {
-  val versionToRelease by option().required().help("The version to release")
+  val versionToRelease by argument()
 
   override fun run() {
     prepareRelease(versionToRelease)
