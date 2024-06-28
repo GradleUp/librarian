682de4aaeeb362f7e3b14a62bdfab47ec0791d1f
Martin Bonnin <martin@mbonnin.net>
2024-06-28 18:58:33 +0200

    Support non KMP android projects (#21)

--- core/src/main/kotlin/com/gradleup/librarian/core/internal/publishing.android.kt
+++ core/src/main/kotlin/com/gradleup/librarian/core/internal/publishing.android.kt
@@ -1,8 +1,12 @@
 package com.gradleup.librarian.core.internal
 
 import com.android.build.api.dsl.LibraryExtension
+import com.android.build.gradle.tasks.SourceJarTask
+import com.gradleup.librarian.core.LIBRARIAN_GENERATE_VERSION
 import com.gradleup.librarian.core.internal.androidExtension
 import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
 
 fun Project.createAndroidPublication(variantName: String) {
   val android = androidExtension
@@ -13,5 +17,16 @@ fun Project.createAndroidPublication(variantName: String) {
     singleVariant(variantName) {
       withSourcesJar()
     }
+  }
+
+  extensions.getByType(PublishingExtension::class.java).apply {
+    publications.register("default", MavenPublication::class.java) { publication ->
+      afterEvaluate {
+        publication.from(components.getByName(variantName))
+      }
+    }
+  }
+  tasks.withType(SourceJarTask::class.java){ task ->
+    task.dependsOn(LIBRARIAN_GENERATE_VERSION)
   }
 }
