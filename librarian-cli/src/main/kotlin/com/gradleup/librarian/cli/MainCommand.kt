2cdb4fa8a36657aec1b52d2c5ec836f04713a80c
Martin Bonnin <martin@mbonnin.net>
2024-07-08 18:01:18 +0200

    Fix host, add --version

--- librarian-cli/src/main/kotlin/com/gradleup/librarian/cli/MainCommand.kt
+++ librarian-cli/src/main/kotlin/com/gradleup/librarian/cli/MainCommand.kt
@@ -2,12 +2,21 @@ package com.gradleup.librarian.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import kotlin.system.exitProcess
 
-internal class MainCommand: CliktCommand() {
+internal class MainCommand: CliktCommand(invokeWithoutSubcommand = true) {
+  val version by option().flag()
+
   init {
     subcommands(PrepareReleaseCommand(), CreateCommand(), ReleaseCommand(), SetupGithubCommand(), InitCommand())
   }
-  override fun run() {
 
+  override fun run() {
+    if (version) {
+      println("librarian $VERSION")
+      exitProcess(1)
+    }
   }
 }
