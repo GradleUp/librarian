c5e5c9b6f5722a655ec3a58b3294dbc5adf85b00
Martin Bonnin <martin@mbonnin.net>
2024-09-16 15:34:07 +0200

    update to more recent librarian

--- librarian-gradle-plugin/testProjects/simple/module/build.gradle.kts
+++ librarian-gradle-plugin/testProjects/simple/module/build.gradle.kts
@@ -1,8 +1,8 @@
+import com.gradleup.librarian.gradle.librarianModule
+
 plugins {
     id("org.jetbrains.kotlin.jvm")
     id("com.gradleup.librarian")
 }
 
-librarian {
-    module()
-}
+librarianModule()
