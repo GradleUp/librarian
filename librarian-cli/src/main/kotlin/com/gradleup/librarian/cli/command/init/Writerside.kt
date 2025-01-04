2e59bab656c29652c6db4abdc350751a63812400
Martin Bonnin <martin@mbonnin.net>
2025-01-04 18:14:52 +0100

    udpate writerside version

--- librarian-cli/src/main/kotlin/com/gradleup/librarian/cli/command/init/Writerside.kt
+++ librarian-cli/src/main/kotlin/com/gradleup/librarian/cli/command/init/Writerside.kt
@@ -5,7 +5,7 @@ import com.gradleup.librarian.core.tooling.GH
 import com.gradleup.librarian.core.tooling.init.initWriterside
 import kotlin.io.path.Path
 
-internal class WriterSideCommand : CliktCommand(help = "Adds an template Writerside documentation to the project") {
+internal class WriterSideCommand : CliktCommand(help = "Adds a Writerside template documentation to the project") {
   override fun run() {
     Path(".").apply {
       val repository = GH().repository()
