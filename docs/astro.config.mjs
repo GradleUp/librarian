7982287c3ff328f3d8878a159a55937f6234bf7b
Martin Bonnin <martin@mbonnin.net>
2025-01-05 18:15:51 +0100

    configure site

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+	site: 'https://gradleup.com/librarian',
 	integrations: [
 		starlight({
 			title: 'Librarian',
