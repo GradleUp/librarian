bf0ef8adfe269864949b14b79007b80941a087d0
Martin Bonnin <martin@mbonnin.net>
2025-06-11 11:52:39 +0200

    internal

--- librarian-gradle-plugin/src/main/kotlin/com/gradleup/librarian/gradle/module.kt
+++ librarian-gradle-plugin/src/main/kotlin/com/gradleup/librarian/gradle/module.kt
@@ -8,23 +8,23 @@ import com.gradleup.librarian.gradle.internal.findEnvironmentVariable
 import org.gradle.api.Project
 import java.util.Properties
 
-fun Properties.javaCompatibility(): Int? {
+internal fun Properties.javaCompatibility(): Int? {
   return getProperty("java.compatibility")?.toInt()
 }
 
-fun Properties.kotlinCompatibility(): String? {
+internal fun Properties.kotlinCompatibility(): String? {
   return getProperty("kotlin.compatibility")
 }
 
-fun Properties.versionPackageName(): String? {
+internal fun Properties.versionPackageName(): String? {
   return getProperty("version.packageName")
 }
 
-fun Properties.publish(): Boolean? {
+internal fun Properties.publish(): Boolean? {
   return getProperty("publish")?.toBoolean()
 }
 
-fun Properties.createMissingPublications(): Boolean? {
+internal fun Properties.createMissingPublications(): Boolean? {
   return getProperty("createMissingPublications")?.toBoolean()
 }
 
@@ -44,7 +44,7 @@ internal fun Properties.olderVersions(): List<Coordinates> {
     }
 }
 
-fun Project.configureMavenFriendlyDependencies() {
+internal fun Project.configureMavenFriendlyDependencies() {
   pluginManager.apply("com.gradleup.maven-sympathy")
 }
 
