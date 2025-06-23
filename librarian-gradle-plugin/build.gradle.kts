08be5be59df494478ab3d8f07819e29e26c12c92
Martin Bonnin <martin@mbonnin.net>
2025-06-23 22:07:14 +0200

    Use gratatouille (#56)

--- librarian-gradle-plugin/build.gradle.kts
+++ librarian-gradle-plugin/build.gradle.kts
@@ -1,44 +1,36 @@
 import com.gradleup.librarian.gradle.Librarian
-import org.gradle.api.internal.artifacts.dependencies.DefaultFileCollectionDependency
 
 plugins {
-    id("org.jetbrains.kotlin.jvm")
-    id("java-gradle-plugin")
+  id("org.jetbrains.kotlin.jvm")
+  id("com.google.devtools.ksp")
+  id("com.gradleup.gratatouille")
 }
 
 Librarian.module(project)
 
 dependencies {
-    api(libs.bcv)
-    api(libs.dokkatoo)
+  api(libs.bcv)
+  api(libs.dokkatoo)
 
-    implementation(project(":librarian-core"))
-    implementation(libs.coroutines)
-    implementation(libs.vespene)
-    implementation(libs.maven.sympathy)
-    implementation(libs.okhttp)
-    implementation(libs.google.auth)
+  implementation(project(":librarian-core"))
+  implementation(libs.coroutines)
+  implementation(libs.vespene)
+  implementation(libs.maven.sympathy)
+  implementation(libs.okhttp)
+  implementation(libs.google.auth)
 
-    compileOnly(libs.gradle.api)
-    compileOnly(libs.agp)
-    compileOnly(libs.kgp)
+  compileOnly(libs.gradle.api)
+  compileOnly(libs.agp)
+  compileOnly(libs.kgp)
 
-    testImplementation(kotlin("test"))
-    testImplementation(gradleTestKit())
-    testImplementation(libs.mockserver)
+  testImplementation(kotlin("test"))
+  testImplementation(gradleTestKit())
+  testImplementation(libs.mockserver)
 }
 
-configurations.getByName("api").dependencies.removeIf {
-    it is DefaultFileCollectionDependency
-}
+gratatouille {
+  pluginMarker("com.gradleup.gratatouille")
+  codeGeneration {
 
-gradlePlugin {
-    plugins {
-        create("com.gradleup.librarian") {
-            id = "com.gradleup.librarian"
-            description =
-                "The librarian plugin. The plugin is no-op and merely serves for plugin the librarian .jar into the build classpath"
-            implementationClass = "com.gradleup.librarian.gradle.LibrarianPlugin"
-        }
-    }
+  }
 }
