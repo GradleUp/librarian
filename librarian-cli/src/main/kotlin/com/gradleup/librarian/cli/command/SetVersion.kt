415216f69636cbe38045080c8743735c275e04ae
Martin Bonnin <martin@mbonnin.net>
2025-07-18 18:54:45 +0200

    Add CLI helpers to bump versions

---
+++ librarian-cli/src/main/kotlin/com/gradleup/librarian/cli/command/SetVersion.kt
@@ -0,0 +1,14 @@
+package com.gradleup.librarian.cli.command
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+
+class SetVersion : CliktCommand() {
+  val version by argument()
+  override fun run() {
+    setCurrentVersion(version)
+
+    println("Version is now '$version'.")
+  }
+}
+
