9f44c412326c38aef286c33c83a2460aa4d34681
Martin Bonnin <martin@mbonnin.net>
2025-12-30 13:37:48 +0100

    Update update-repo.main.kts

--- scripts/update-repo.main.kts
+++ scripts/update-repo.main.kts
@@ -1,12 +1,14 @@
 #!/usr/bin/env kotlin
 
 @file:Repository("https://repo.maven.apache.org/maven2/")
+@file:Repository("https://dl.google.com/android/maven2/")
 @file:Repository("https://storage.googleapis.com/gradleup/m2")
 @file:Repository("https://jitpack.io")
-
-@file:DependsOn("com.gradleup.librarian:librarian-cli:0.0.11-SNAPSHOT-e8ab4653f68118d36889519f430f1fcd118745e3")
+//@file:Repository("file://~/.m2/repository")
+@file:DependsOn("com.gradleup.librarian:librarian-cli:0.2.2-SNAPSHOT-0cbca80f60a15a0f851a9cd468bfc352db316dd4")
 
 import com.gradleup.librarian.cli.updateRepo
+
 
 updateRepo(args) {
   file("README.md") {
