c77a3ec209227a572acc25e439f39a12a5e6614f
Martin Bonnin <martin@mbonnin.net>
2025-12-31 12:32:19 +0100

    Allow to tweak how the version is set (#115)

--- librarian-cli/src/main/kotlin/com/gradleup/librarian/cli/command/SetVersion.kt
+++ librarian-cli/src/main/kotlin/com/gradleup/librarian/cli/command/SetVersion.kt
@@ -3,10 +3,12 @@ package com.gradleup.librarian.cli.command
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
 
-internal class SetVersion : CliktCommand() {
+internal class SetVersion(val setVersion: VersionContext.() -> Unit) : CliktCommand() {
   val version by argument()
   override fun run() {
     setCurrentVersion(version)
+
+    VersionContext(version).setVersion()
 
     println("Version is now '$version'.")
   }
