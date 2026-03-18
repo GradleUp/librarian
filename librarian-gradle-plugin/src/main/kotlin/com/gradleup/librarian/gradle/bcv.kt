51318fc735e5cfc561c7cb41bb22c1d7647d0fdb
Martin Bonnin <martin@mbonnin.net>
2026-03-18 12:51:49 +0100

    Support both KGP 2.3.0 and 2.3.20 at the same time (#123)

--- librarian-gradle-plugin/src/main/kotlin/com/gradleup/librarian/gradle/bcv.kt
+++ librarian-gradle-plugin/src/main/kotlin/com/gradleup/librarian/gradle/bcv.kt
@@ -3,10 +3,9 @@
 package com.gradleup.librarian.gradle
 
 import com.gradleup.librarian.core.tooling.semVerOrThrow
+import com.gradleup.librarian.internal.configureBcv220
+import com.gradleup.librarian.internal.configureBcv2320
 import org.gradle.api.Project
-import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
-import org.jetbrains.kotlin.gradle.dsl.abi.AbiValidationExtension
-import org.jetbrains.kotlin.gradle.dsl.abi.AbiValidationMultiplatformExtension
 import org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation
 import org.jetbrains.kotlin.gradle.plugin.getKotlinPluginVersion
 
@@ -14,57 +13,13 @@ fun Project.configureBcv(
   warnIfMissing: Boolean = true,
   excludePatterns: List<String> = emptyList(),
 ) {
-  extensions.getByType(KotlinProjectExtension::class.java).apply {
-    val abiValidation = this.extensions.findByName("abiValidation")
+  // See https://kotlinlang.org/docs/whatsnew2320.html#improvements-to-binary-compatibility-validation-in-kgp
+  val kgpVersion = this@configureBcv.getKotlinPluginVersion().semVerOrThrow()
 
-    if (abiValidation != null) {
-      when (abiValidation) {
-        is AbiValidationExtension -> {
-          abiValidation.enabled.set(true)
-          abiValidation.filters {
-            it.exclude.byNames.addAll(excludePatterns)
-          }
-        }
-        is AbiValidationMultiplatformExtension -> {
-          abiValidation.enabled.set(true)
-          abiValidation.filters {
-            it.exclude.byNames.addAll(excludePatterns)
-          }
-        }
-        else -> error("Librarian: unknown abiValidation extension type: '${abiValidation.javaClass.name}'")
-      }
-
-      // See https://kotlinlang.org/docs/whatsnew2320.html#improvements-to-binary-compatibility-validation-in-kgp
-      val kgpVersion = this@configureBcv.getKotlinPluginVersion().semVerOrThrow()
-
-      /**
-       * Compatibility tasks to not break the brain muscle
-       */
-      if (kgpVersion >= "2.3.20".semVerOrThrow()) {
-        tasks.register("apiDump") {
-          it.dependsOn("updateKotlinAbi")
-          it.doLast {
-            println("`apiDump` is deprecated. Use `updateKotlinAbi` instead")
-          }
-        }
-        tasks.register("apiCheck") {
-          it.dependsOn("checkKotlinAbi")
-          it.doLast {
-            println("`apiCheck` is deprecated. Use `updateKotlinAbi` instead")
-          }
-        }
-      } else {
-        tasks.named("build") {
-          it.dependsOn("checkLegacyAbi")
-        }
-        tasks.register("apiDump") {
-          it.dependsOn("updateLegacyAbi")
-        }
-        tasks.register("apiCheck") {
-          it.dependsOn("checkLegacyAbi")
-        }
-      }
-    } else {
+  when {
+    kgpVersion >= "2.3.20".semVerOrThrow() -> configureBcv2320(excludePatterns)
+    kgpVersion >= "2.2.0".semVerOrThrow() -> configureBcv220(excludePatterns)
+    else -> {
       if (warnIfMissing) {
         println("Librarian: BCV is only configured by default if using KGP 2.2+ (currently detected is '${getKotlinPluginVersion()}'). Set bcv.warn=false in your librarian.root.properties file to remove this warning.")
       }
